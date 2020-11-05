0464d438c5ac51ff53fb4aca81172f694339e620
brandongarrett <72109485+brandongarrett@users.noreply.github.com>
2020-11-05 14:59:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
-const skyImg = new Image()
-sky.src = `./images/sky.jpg`
 
-//Three objects that we can manipulate
-let sky = { x: 0, y: 0, w: canvas.width, h: canvas.height }
+const skyImg = new Image();
+skyImg.src = `./images/sky.jpg`;
 
-let ship = { x: (canvas.width / 2) - (40), y: canvas.height - 140, w: 80, h: 140 }
+const shipImg = new Image();
+shipImg.src = `./images/enterprise.png`;
 
+canvas.width = window.innerWidth * .98;
+canvas.height = canvas.width;
 
-
-//Need to wait until image loads before drawing.  
-skyImg.onload = function () {
-    ctx.drawImage(skyImg, sky.x, sky.y, sky.w, sky.h)
+let sky = {
+    x: 0,
+    y: 0,
+    w: canvas.width,
+    h: canvas.height
 }
 
-const shipImg = new Image()
-shipImg.src = `./images/ship.jpg`
-
-//Need to wait until image loads before drawing.  
-shipImg.onload = function () {
-    ctx.shipImg(shipImg, ship.x, ship.y, ship.w, ship.h)
+class Ship {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
 }
